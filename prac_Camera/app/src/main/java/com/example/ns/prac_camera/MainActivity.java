aceb88b866823a5f3aceacb771698f2d6506c948
namsoo <are34@naver.com>
2019-01-12 18:04:52 +0900

    카메라 가로저장 버그 오류수정

--- prac_Camera/app/src/main/java/com/example/ns/prac_camera/MainActivity.java
+++ prac_Camera/app/src/main/java/com/example/ns/prac_camera/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
@@ -33,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     ImageView iv;
     String filename; //파일이름
     Uri uri;
+    String photoFile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
                 }
                 SimpleDateFormat dateFormat =new SimpleDateFormat("yyyyMMddhhmmss");
                 String date = dateFormat.format(new Date());  //사진이름을 시간으로구분
-                String photoFile = "테스트"+date+".jpg";   //사진이름
+                photoFile = "테스트"+date+".jpg";   //사진이름
                 filename = pictureFileDir.getPath()+File.separator+photoFile;
                 uri = FileProvider.getUriForFile(getApplicationContext(), "com.example.ns.prac_camera.fileprovider", new File(filename));
                 intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
@@ -73,18 +75,33 @@ public class MainActivity extends AppCompatActivity {
         if(requestCode == 100 ) {
             if(resultCode == RESULT_OK) {
 
-                //iv.setImageURI(uri);    //이미지뷰에 사진넣기
+                //해당파일 스캔하여 갤러리에 보이도록 해줌
+                sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.parse("file://" + Environment.getExternalStorageDirectory()+"/DCIM/test/"+photoFile)));
 
                 Glide.with(getApplicationContext())
                         .load(uri)
                         .into(iv);
 
+                //iv.setImageURI(uri);    //이미지뷰에 사진넣기
+                //기본카메라어플사용시 null오류가 일어나므로 강제로 uri를얻어와서 지정함
+/*
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 8; //   1/8크기로 비트맵만들어줌
+                Bitmap bitmap = BitmapFactory.decodeFile(filename, options);
+                iv.setImageBitmap(bitmap);
+*/
 
                 /* 갤러리에 보이게해주는 코드  */
-                ContentValues values = new ContentValues();
+               // 사용시 사진이 가로로 보이는 오류발생
+        /*        ContentValues values = new ContentValues();
                 values.put(MediaStore.Images.Media.DATA, filename);
                 values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpg");
                 getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+*/
+
+                //MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, "title", "descripton");
+
+
             }
         }
     }
