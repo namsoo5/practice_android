3e8c44b395b06191073baf42f60bb949f56c2a8f
namsoo <are34@naver.com>
2019-01-13 17:24:41 +0900

    viewpager

---
+++ prac_pager/app/src/main/java/com/example/ns/prac_pager/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.ns.prac_pager;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ViewPager pager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pager = findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(3); //뒤에안보이는화면 캐싱
+
+        final MoviePagerAdapter adapter = new MoviePagerAdapter(getSupportFragmentManager());
+
+        Fragment1 fragment1 = new Fragment1();
+        adapter.addItem(fragment1);
+
+        Fragment2 fragment2 = new Fragment2();
+        adapter.addItem(fragment2);
+
+        Fragment3 fragment3 = new Fragment3();
+        adapter.addItem(fragment3);
+
+        pager.setAdapter(adapter);
+
+
+        Button bt = findViewById(R.id.button);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pager.setCurrentItem(1);
+            }
+        });
+
+    }
+    class MoviePagerAdapter extends FragmentStatePagerAdapter{
+
+        ArrayList<Fragment> items = new ArrayList<Fragment>();
+
+        public MoviePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addItem(Fragment item){
+            items.add(item);
+        }
+        @Override
+        public Fragment getItem(int position) {
+
+            return items.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Nullable
+        @Override  //페이지마다 타이틀붙여서 리턴
+        public CharSequence getPageTitle(int position) {
+            return "페이지 "+position;
+        }
+    }
+}
